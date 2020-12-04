f0d9006d2a4bfb2c48ff4759956777afd4c79298
Ankush <ankushpndt@gmail.com>
2020-12-04 21:00:31 +0530

    finished

---
+++ index.js
@@ -0,0 +1,16 @@
+var input = document.querySelector("#txt-area");
+var btn = document.querySelector("#btn-translate");
+var output = document.querySelector("#output-txt");
+
+function clickHandler(){
+    var a = input.value;
+    fetch("https://api.funtranslations.com/translate/ferb-latin.json?text="+a)
+    .then(resolve => resolve.json())
+    .then(json => {
+        var translatedoutput = json.contents.translated;
+        output.innerText = translatedoutput;
+    })
+    .catch(error => console.log(error))
+}
+
+btn.addEventListener("click", clickHandler);
