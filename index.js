81a6ee38059d10c6bd4531d49d0675d40523ef38
Ankush <ankushpndt@gmail.com>
2020-12-20 18:17:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var output = document.querySelector("#output-txt");
 function clickHandler(){
     var a = input.value;
     fetch("https://api.funtranslations.com/translate/ferb-latin.json?text="+a)
-    .then(resolve => resolve.json())
+    .then(response => response.json())
     .then(json => {
         var translatedoutput = json.contents.translated;
         output.innerText = translatedoutput;
